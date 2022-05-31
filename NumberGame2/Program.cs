de990d46fa14aecd140cb4cd3ecef4c2b6e9c6e7
CorbinMakesStuff <79483694+CorbinMakesStuff@users.noreply.github.com>
2022-05-31 11:41:40 -0700

    bug fix
    fix bug where the game would crash if the low value is higher than the high value

--- NumberGame2/Program.cs
+++ NumberGame2/Program.cs
@@ -10,46 +10,57 @@ namespace NumberGame2
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.Title = "NumberGame";
-            Random rn = new Random();
-            Console.WriteLine("Please set your number parameters.");
-            Console.Write("Low: ");
-            string lo = Console.ReadLine();
-            Console.WriteLine();
-            Console.Write("High: ");
-            string hi = Console.ReadLine();
-            Console.Clear();
-            int loi = Convert.ToInt32(lo);
-            int hii = Convert.ToInt32(hi);
-            int ran = rn.Next(loi, hii);
-            int guessint;
-            Console.WriteLine("Guess a number!");
-            int guesscount = 1;
             do
             {
-                Console.Write("Guess: ");
-                string guess = Console.ReadLine();
-                guessint = Convert.ToInt32(guess);
                 Console.Clear();
-                if (guessint == ran)
+                Console.Title = "NumberGame";
+                Random rn = new Random();
+                Console.WriteLine("Please set your number parameters.");
+                Console.Write("Low: ");
+                string lo = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("High: ");
+                string hi = Console.ReadLine();
+                Console.Clear();
+                int loi = Convert.ToInt32(lo);
+                int hii = Convert.ToInt32(hi);
+                if (loi >= hii)
                 {
-                    Console.WriteLine("Correct!");
-                    break;
+                    Console.WriteLine("The low value cannot be higher than the high value!");
+                    Console.ReadKey();
                 }
-                else if (guessint > ran)
+                else if (hii > loi)
                 {
-                    Console.WriteLine("Lower!");
-                    guesscount++;
+                    int ran = rn.Next(loi, hii);
+                    int guessint;
+                    Console.WriteLine("Guess a number!");
+                    int guesscount = 1;
+                    do
+                    {
+                        Console.Write("Guess: ");
+                        string guess = Console.ReadLine();
+                        guessint = Convert.ToInt32(guess);
+                        Console.Clear();
+                        if (guessint == ran)
+                        {
+                            Console.WriteLine("Correct!");
+                            break;
+                        }
+                        else if (guessint > ran)
+                        {
+                            Console.WriteLine("Lower!");
+                            guesscount++;
+                        }
+                        else if (guessint < ran)
+                        {
+                            Console.WriteLine("Higher!");
+                            guesscount++;
+                        }
+                    } while (guessint != ran);
+                    Console.WriteLine("It took you " + guesscount + " guesses!");
+                    Console.ReadKey();
                 }
-                else if (guessint < ran)
-                {
-                    Console.WriteLine("Higher!");
-                    guesscount++;
-                }
-            } while (guessint != ran);
-            Console.WriteLine("It took you " + guesscount + " guesses!");
-            Console.ReadKey();
+            } while (true);
         }
     }
 }
