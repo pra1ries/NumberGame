dccb96b9821d25b83de29fd598811124bd4c2596
CorbinMakesStuff <79483694+CorbinMakesStuff@users.noreply.github.com>
2022-09-21 12:10:29 -0700

    add comments to source code

--- NumberGame2/Program.cs
+++ NumberGame2/Program.cs
@@ -18,58 +18,45 @@ namespace NumberGame2
                 Console.WriteLine("Please set your number parameters.");
                 Console.Write("Low: ");
                 string lo = Console.ReadLine();
-                if (lo == "")
-                {
-                    Console.WriteLine("You cannot have a null value!");
-                    Console.ReadKey();
-                    Environment.Exit(0);
-                }
                 Console.WriteLine();
                 Console.Write("High: ");
                 string hi = Console.ReadLine();
-                if (hi == "")
-                {
-                    Console.WriteLine("You cannot have a null value!");
-                    Console.ReadKey();
-                    Environment.Exit(0);
-                }
                 Console.Clear();
-                int loi = Convert.ToInt32(lo);
-                int hii = Convert.ToInt32(hi);
-                if (loi >= hii)
+                int loi = Convert.ToInt32(lo); // converts lo string to int
+                int hii = Convert.ToInt32(hi); // converts hi string to int
+                if (loi >= hii) // checks to make sure low value is not higher than the high value
                 {
-                    Console.WriteLine("The low value cannot be higher than or equal to the high value!");
-                    Console.ReadKey();
+                    Console.WriteLine("The low value cannot be higher than or equal to the high value!"); // if low value is higher than the high value, return this message.
+                    Console.ReadKey(); // wait for a key to be pressed, then close application
                 }
                 else if (hii > loi)
                 {
-                    int ran = rn.Next(loi, hii);
+                    int ran = rn.Next(loi, hii); // generates random number according to the parameters set earlier
                     int guessint;
                     Console.WriteLine("Guess a number!");
-                    int guesscount = 1;
+                    int guesscount = 1; 
                     do
                     {
                         Console.Write("Guess: ");
-                        string guess = Console.ReadLine();
-                        guessint = Convert.ToInt32(guess);
+                        string guess = Console.ReadLine(); // saves guess
+                        guessint = Convert.ToInt32(guess); // converts guess to integer
                         Console.Clear();
-                        if (guessint == ran)
+                        if (guessint == ran) // checks to see if answer is correct
                         {
-                            Console.Title = "NumberGame - Correct!";
                             Console.WriteLine("Correct!");
                             break;
                         }
-                        else if (guessint > ran)
+                        else if (guessint > ran) // checks to see if guess is higher than answer
                         {
                             Console.Title = "NumberGame - Lower!";
                             Console.WriteLine("Lower!");
-                            guesscount++;
+                            guesscount++; // adds 1 guess to guesscount
                         }
-                        else if (guessint < ran)
+                        else if (guessint < ran) // checks to see if guess is lower than answer
                         {
                             Console.Title = "NumberGame - Higher!";
                             Console.WriteLine("Higher!");
-                            guesscount++;
+                            guesscount++; // adds 1 guess to guesscount
                         }
                     } while (guessint != ran);
                     Console.Title = "NumberGame";
